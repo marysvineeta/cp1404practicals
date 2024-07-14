49864176c109e9dc24fa481bd39f2173cb28bb6f
marys003 <marysvineeta.paul@my.jcu.edu.au>
2024-07-14 21:40:56 +1000

    Adding limo car object

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -14,5 +14,20 @@ def main():
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
 
+    # Create a new Car object called "limo" that is initialised with 100 units of fuel.
+    limo = Car(100)
+
+    # Add 20 more units of fuel to this new car object using the add method.
+    limo.add_fuel(20)
+
+    # Print the amount of fuel in the car.
+    print(f"Limo has fuel: {limo.fuel}")
+
+    # Attempt to drive the car 115 km using the drive method.
+    limo.drive(115)
+
+
+    print(limo)
+
 
 main()
