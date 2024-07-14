ce922e24ac91cfc2a7164ca2a4118ea380bd652f
marys003 <marysvineeta.paul@my.jcu.edu.au>
2024-07-14 22:09:39 +1000

    add car file

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,13 +9,13 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("My Car", 180)
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
 
     # Create a new Car object called "limo" that is initialised with 100 units of fuel.
-    limo = Car(100)
+    limo = Car("Limo", 100)
 
     # Add 20 more units of fuel to this new car object using the add method.
     limo.add_fuel(20)
@@ -26,7 +26,7 @@ def main():
     # Attempt to drive the car 115 km using the drive method.
     limo.drive(115)
 
-
+    # Print the limo's details after the drive attempt.
     print(limo)
 
 
