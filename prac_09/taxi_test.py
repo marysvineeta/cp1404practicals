3df2f10ae69af83f3660aa26d3ea7c1d36117077
marys003 <marysvineeta.paul@my.jcu.edu.au>
2024-08-19 03:37:36 +1000

    add file for taxi

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -3,12 +3,9 @@ from taxi import Taxi
 
 
 def main():
-
-    my_taxi = Taxi("Prius 1", 100, 1.23)
-
+    my_taxi = Taxi("Prius 1", 100)
 
     my_taxi.drive(40)
-
 
     print(my_taxi)
     print(f"Current fare: ${my_taxi.get_fare():.2f}")
